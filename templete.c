1f5cc5dc786e1eff6fbfcd5e20c2ac9df47f3aa3
anud18 <as12345033@gmail.com>
2022-05-02 20:38:26 +0800

    220502

---
+++ templete.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
+#define ABS(a) ((a) > 0) ? (a) : (-a)
+int main(){
+
+}
